f51749053d5d2454fc3ddb194aff73c6568750ac
Theodros Yimer <theox01@hotmail.fr>
2023-04-02 01:41:26 +0200

    feat: add api calls

--- src/tmdb.js
+++ src/tmdb.js
@@ -1,28 +1,95 @@
-export const searchByID = async (id) => {
-  let response = await fetch(`${import.meta.env.VITE_BASE_URL}/movie/${id}?api_key=${import.meta.env.VITE_API_KEY}&append_to_response=videos,images`).catch((e) => {
-    if (!response.ok) {
-      throw new Error(`Returned with a ${response.status} code`)
-    }
-    console.error(e.message)
-  })
+export const searchByID = async ({ id, type, lang }) => {
+  let response = await fetch(`${import.meta.env.VITE_BASE_URL}/${type}/${id}?api_key=${import.meta.env.VITE_API_KEY}&append_to_response=videos,images,credits&include_image_language=${lang.replace(/-(\w+)/g, '')}&include_video_language=${lang.slice(0, 2)}&language=${lang}`)
+    .catch((e) => {
+      if (!response.ok) {
+        throw new Error(`Returned with a ${response.status} code`)
+      }
+      console.error(e.message)
+    })
 
   let data = await response.json()
-  return data
+
+  return { data }
 }
 
-export const searchByTitle = async (query) => {
-  let response = await fetch(`${import.meta.env.VITE_BASE_URL}/search/movie?api_key=${import.meta.env.VITE_API_KEY}&query=${query}`).catch((e) => {
-    if (!response.ok) {
-      throw new Error(`Returned with a ${response.status} code`)
-    }
-    console.error(e.message)
-  })
+export const searchByTitle = async ({ query, type = 'movie', lang = 'fr-FR' }) => {
+  let response = await fetch(`${import.meta.env.VITE_BASE_URL}/search/${type}?api_key=${import.meta.env.VITE_API_KEY}&query=${query}&language=${lang}`)
+    .catch((e) => {
+      if (!response.ok) {
+        throw new Error(`Returned with a ${response.status} code`)
+      }
+      console.error(e.message)
+    })
 
-  let data = await response.json()
-  return data.results
+  let { page: pageCurrent, results, total_pages, total_results } = await response.json()
+
+  return { page: pageCurrent, results, total_pages, total_results }
 }
 
-export function getTopRatedFilms() {
+export const getTopRated = async ({ type = 'movie', page = 1, lang = 'fr-FR' }) => {
+  let response = await fetch(`${import.meta.env.VITE_BASE_URL}/${type}/top_rated?api_key=${import.meta.env.VITE_API_KEY}&language=${lang}&page=${page}`)
+    .catch((e) => {
+      if (!response.ok) {
+        throw new Error(`Returned with a ${response.status} code`)
+      }
+      console.error(e.message)
+    })
+
+  let { page: pageCurrent, results, total_pages, total_results } = await response.json()
+
+  return { page: pageCurrent, results, total_pages, total_results }
+}
+
+export const getPopular = async ({ type = 'movie', page = 1, lang = 'fr-FR' }) => {
+  let response = await fetch(`${import.meta.env.VITE_BASE_URL}/${type}/popular?api_key=${import.meta.env.VITE_API_KEY}&language=${lang}&page=${page}`)
+    .catch((e) => {
+      if (!response.ok) {
+        throw new Error(`Returned with a ${response.status} code`)
+      }
+      console.error(e.message)
+    })
+
+  let { page: pageCurrent, results, total_pages, total_results } = await response.json()
+
+  return { page: pageCurrent, results, total_pages, total_results }
+}
+
+export const getUpcoming = async ({ type = 'movie', page = 1, lang = 'fr-FR' }) => {
+  let response = await fetch(`${import.meta.env.VITE_BASE_URL}/${type}/upcoming?api_key=${import.meta.env.VITE_API_KEY}&language=${lang}&page=${page}`)
+    .catch((e) => {
+      if (!response.ok) {
+        throw new Error(`Returned with a ${response.status} code`)
+      }
+      console.error(e.message)
+    })
+
+  let { dates, page: pageCurrent, results, total_pages, total_results } = await response.json()
+
+  return {
+    dates,
+    page: pageCurrent,
+    results,
+    total_pages,
+    total_results
+  }
+}
+
+export const fetchGenresList = async ({ type = 'movie', lang = 'fr-FR' }) => {
+  let response = await fetch(`${import.meta.env.VITE_BASE_URL}/genre/${type}/list?api_key=${import.meta.env.VITE_API_KEY}&language=${lang}`)
+    .catch((e) => {
+      if (!response.ok) {
+        throw new Error(`Returned with a ${response.status} code`)
+      }
+      console.error(e.message)
+    })
+
+  let { genres } = await response.json()
+
+  return genres
+}
 
 
+export const fetchTvAndMovieGenresList = async (lang = 'fr-FR') => {
+  const [movieGenresList, tvGenresList] = await Promise.all([fetchGenresList({ lang }), fetchGenresList({ type: 'tv', lang })])
+  console.log(movieGenresList, tvGenresList)
 }
