62b48625c96e5c06612a53e45dd7be3cb7a74f16
Theodros Yimer <theox01@hotmail.fr>
2023-04-02 22:50:07 +0200

    feat: add dropdowns and handle functions

--- src/ui.js
+++ src/ui.js
@@ -8,8 +8,29 @@ export function handleLinks(page) {
 
 export function handleSelectGenresAndSortBy(page) {
   const selectSortAndGenreElement = document.querySelector('#sort-genres-group')
+  const dropdowns = [...document.querySelectorAll('#sort-genres-group .dropdown')]
 
   if (page !== 'home') {
     selectSortAndGenreElement.style.opacity = 1
   }
+
+  selectSortAndGenreElement.addEventListener('click', e => {
+    if (e.target.matches('#sort-genres-group > button')) {
+      let currentDropdown = document.querySelector(`button[data-action="${e.target.dataset.action}"] .dropdown`)
+
+      if (currentDropdown.classList.contains('show')) {
+        currentDropdown.classList.remove('show')
+        return
+      }
+      dropdowns.forEach(dropdown => dropdown.classList.remove('show'))
+
+      currentDropdown.classList.add('show')
+    }
+
+
+  })
+}
+
+export function handleCardBtns() {
+
 }
