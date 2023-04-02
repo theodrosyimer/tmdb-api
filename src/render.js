d017669668d2189c8bdf311b8575e8f770e572e8
Theodros Yimer <theox01@hotmail.fr>
2023-04-02 22:48:12 +0200

    refactor: refactor render function

--- src/render.js
+++ src/render.js
@@ -1,14 +1,23 @@
 import { FilmCards } from "./components/FilmCards.js"
 import { FilmDetails } from "./components/FilmDetails.js"
+import { searchByID } from "./tmdb.js"
 
-export async function render(parentElement, params, callbackAsync) {
+/**
+ *
+ * @param {*} parentElement
+ * @param {*} params
+ * @param {*} callbackAsync
+ * @param {'poster' | 'backdrop'} imageType
+ */
+export async function render(parentElement, params, callbackAsync, imageType) {
   const { id, query, type, lang, page } = params
-  parentElement.innerHTML = await FilmCards(params, callbackAsync)
+  parentElement.innerHTML = await FilmCards(params, callbackAsync, imageType)
 
   parentElement.addEventListener('click', async e => {
     if (e.target.offsetParent.offsetParent.matches('div[data-id]')) {
 
-      let data = await FilmDetails({ id: e.target.offsetParent.offsetParent.dataset.id, type, lang })
+      // let data = await FilmDetails({ id: e.target.offsetParent.offsetParent.dataset.id, type, lang })
+      let { data } = await searchByID({ id: e.target.offsetParent.offsetParent.dataset.id, type, lang })
       console.log(data)
     }
   })
